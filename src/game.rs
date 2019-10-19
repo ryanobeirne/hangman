d52ed2f926291513811f4ddfb02210b049efacf6
Ryan O'Beirne <ryanobeirne@gmail.com>
2019-10-19 18:01:38 -0400

    Read/Writify

--- src/game.rs
+++ src/game.rs
@@ -15,41 +15,47 @@ impl Game {
         }
     }
 
-    pub fn play(&mut self) -> GameOutcome {
+    pub fn play<W: Write, R: BufRead>(&mut self, w: &mut W, r: &mut R) -> io::Result<GameOutcome> {
         loop {
-            println!("{}", self);
+            writeln!(w, "{}", self)?;
 
-            let guess = self.guess(&stdin());
+            let guess = self.guess(r, w)?;
             self.insert(guess);
 
             if let PlayOutcome::GameOver(outcome) = self.check_game() {
                 stdout().flush().expect("Problem writing to stdout!");
-                break outcome;
+                break Ok(outcome);
             }
         }
     }
 
-    fn guess(&mut self, input: &Stdin) -> Letter {
-        print!("Guess a letter: ");
-        stdout().flush().expect("Problem writing to stdout!");
+    fn guess<R: BufRead, W: Write>(&mut self, r: &mut R, w: &mut W) -> io::Result<Letter> {
+        write!(w, "Guess a letter: ")?;
+        w.flush().expect("Problem writing to stdout!");
 
         let mut buf = String::new();
-        input
-            .read_line(&mut buf)
-            .expect("Problem reading from stdin!");
+        r.read_line(&mut buf)?;
+        //let mut bytes = r.bytes().filter_map(|b| b.ok());
+        //while let Some(b) = bytes.next() {
+            //if b == 0x0A || b == 0x0D {
+                //break;
+            //} else {
+                //buf.push(b as char);
+            //}
+        //}
 
         match Letter::try_from(buf.as_str()) {
             Ok(letter) => {
                 if self.contains(&letter) {
-                    println!("You guessed '{}' already!", &letter);
-                    self.guess(&stdin())
+                    writeln!(w, "You guessed '{}' already!", &letter)?;
+                    self.guess(r, w)
                 } else {
-                    letter
+                    Ok(letter)
                 }
             }
             Err(_) => {
-                println!("Invalid guess!");
-                self.guess(&stdin())
+                writeln!(w, "Invalid guess: {:#?}", &buf.trim())?;
+                self.guess(r, w)
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeSet;
 use std::fmt;
-use std::io::{stdin, stdout, Stdin, Write};
+use std::io::{self, stdin, stdout, BufRead, BufReader, Write};
 
 mod game;
 use game::*;
@@ -18,13 +18,14 @@ mod words;
 use words::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hangman!");
-
     let mut output = stdout();
+    let mut input = BufReader::new(stdin());
+
+    write!(&mut output, "Hangman!")?;
 
     let mut multi = MultiGame::new();
 
-    while let GameOutcome::Win(score) = play_new_game() {
+    while let GameOutcome::Win(score) = play_new_game(&mut output, &mut input)? {
         multi.game_count += 1;
         multi.score += score;
         multi.status(&mut output)?;
@@ -35,20 +36,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn play_new_game() -> GameOutcome {
+fn play_new_game<W: Write, R: BufRead>(w: &mut W, r: &mut R) -> io::Result<GameOutcome> {
     let word = match std::env::var("WORD") {
         Ok(word) => Word::try_from(word.as_str()).unwrap_or(rand_word()),
         Err(_) => rand_word(),
     };
-    println!("{} letters:", word.len());
+
+    writeln!(w, "{} letters:", word.len())?;
 
     let mut game = Game::new(&word);
 
-    let outcome = game.play();
+    let outcome = game.play(w, r)?;
 
-    println!("{}\n{}\n{}", game, outcome, word);
+    writeln!(w, "{}\n{}\n{}", game, outcome, word)?;
 
-    outcome
+    Ok(outcome)
 }
 
 #[derive(Debug)]
@@ -65,7 +67,7 @@ impl MultiGame {
         }
     }
 
-    fn status<W: Write>(&self, w: &mut W) -> std::io::Result<()> {
+    fn status<W: Write>(&self, w: &mut W) -> io::Result<()> {
         writeln!(w, "Games: {} | Score: {}", self.game_count, self.score)
     }
 }
